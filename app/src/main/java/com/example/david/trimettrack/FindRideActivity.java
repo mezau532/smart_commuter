e9dccbb0c3018d21d958b69764b2a70823cb1151
Ulysses Meza <umeza@pdx.edu>
2017-07-28 22:33:09 -0700

    app/.

--- app/src/main/java/com/example/david/trimettrack/FindRideActivity.java
+++ app/src/main/java/com/example/david/trimettrack/FindRideActivity.java
@@ -9,9 +9,18 @@ import Sync.Info.UberCostEstimateDTO;
 import Sync.Info.UberListOfCostEstimateDTOs;
 import Sync.UberSync;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -48,6 +57,10 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class FindRideActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private LocationListener listener;
+    double lat;
+    double lng;
     private CheckBox locationCheckbox;
     private EditText startAdressInput;
     //don't know if I want these private or not?
@@ -70,15 +83,72 @@ public class FindRideActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_ride);
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        configure_button();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+        }
     }
 
     public void onCheckboxClicked(View view){
+        configure_button();
         boolean checked = ((CheckBox) view).isChecked();
         switch (view.getId()){
             case R.id.LocationCheckBox:
                 if(checked){
                     startAdressInput = (EditText) findViewById(R.id.StartInputBox);
-                    startAdressInput.setText("current location");
+                    startAdressInput.setText(MessageFormat.format("lat: {0} lng:{1}", lat, lng));
                 }
                 else{
                     startAdressInput = (EditText) findViewById(R.id.StartInputBox);
