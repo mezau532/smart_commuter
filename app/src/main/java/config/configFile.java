1fb6e5e0db53d870daa01265a772e8ab5c953d40
Ulysses Meza <umeza@pdx.edu>
2017-08-09 21:24:22 -0700

    add files

---
+++ app/src/main/java/config/configFile.java
@@ -0,0 +1,12 @@
+package config;
+
+/**
+ * Created by umeza on 8/9/17.
+ */
+
+public class configFile {
+    public String trimetApiKey;
+    public String googleApiKey;
+    public String liftApiKey;
+    public String uberApiKey;
+}
