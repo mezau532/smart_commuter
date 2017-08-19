366cd6de05303a03930891aab444ff8411c62ca1
mezau532 <umeza@pdx.edu>
2017-08-18 22:49:37 -0700

    Update LocationDTO.java

--- app/src/main/java/Sync/Info/LocationDTO.java
+++ app/src/main/java/Sync/Info/LocationDTO.java
@@ -1,5 +1,9 @@
 package Sync.Info;
-
+/**
+ * Copyright (c) 2017 Ulysses Meza and Jinghui Han
+ * This code is available under the "MIT License".
+ * Please see the file LICENSE in this distribution for license terms
+ */
 import static okhttp3.internal.Internal.instance;
 
 /**
